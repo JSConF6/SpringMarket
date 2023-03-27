79b5861d6aa69ae2edf557dbbb02ba7dcd5a511f
JSConF <arare30030@naver.com>
2023-03-27 22:33:27 +0900

    스프링 마켓 디자인 완료

---
+++ src/main/java/com/spring/market/TestController.java
@@ -0,0 +1,78 @@
+package com.spring.market;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "auth/signin";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "auth/signup";
+    }
+
+    @GetMapping("/findId")
+    public String findId() {
+        return "findId";
+    }
+
+    @GetMapping("/findPw")
+    public String findPw() {
+        return "findPw";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+
+    @GetMapping("/product/productDetail")
+    public String productDetail() {
+        return "product/productDetail";
+    }
+
+    @GetMapping("/product/productEdit")
+    public String productEdit() {
+        return "product/productEdit";
+    }
+
+    @GetMapping("/product/productAdd")
+    public String productAdd() {
+        return "product/productAdd";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "mypage/mypage";
+    }
+
+    @GetMapping("/mypage/profileEdit")
+    public String profileEdit() {
+        return "mypage/profileEdit";
+    }
+
+    @GetMapping("/mypage/wishList")
+    public String widhList() {
+        return "mypage/wishList";
+    }
+
+    @GetMapping("/mypage/mySaleList")
+    public String mySaleList() {
+        return "mypage/mySaleList";
+    }
+
+    @GetMapping("/mypage/myBuyList")
+    public String myBuyList() {
+        return "mypage/myBuyList";
+    }
+}
