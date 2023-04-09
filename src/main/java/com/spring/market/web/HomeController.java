e63adc410a8e10fc61aaf9972de6a83613354656
JSConF <arare30030@naver.com>
2023-04-09 17:26:06 +0900

    채팅 디자인 완료

--- src/main/java/com/spring/market/web/HomeController.java
+++ src/main/java/com/spring/market/web/HomeController.java
@@ -4,6 +4,7 @@ import com.spring.market.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,5 +25,15 @@ public class HomeController {
     @GetMapping("/findPw")
     public String findPw() {
         return "findPw";
+    }
+
+    @GetMapping("/chatList")
+    public String chatList() {
+        return "chat/chatList";
+    }
+
+    @GetMapping("/chat/{otherUserId}")
+    public String chat(@PathVariable("otherUserId") int otherUserId) {
+        return "chat/chat";
     }
 }
